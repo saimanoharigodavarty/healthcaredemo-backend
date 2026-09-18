ff23332ea84d9c0870a0697484baee399486f818
saimanoharigodavarty <saimanoharigodavarty06@gmail.com>
2026-09-18 22:58:45 +0530

    testing

--- src/models/models.js
+++ src/models/models.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema({
     },
     role:{
         type:String,
-        enum:["patient","receptionist"],
+        enum:["patient","doctor"],
         required:true
     },
     password:{
